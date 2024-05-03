4ca8c0c7c541248cbf0b580c485a853e1d6621c9
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-05-03 08:32:07 +0800

    fix: update btc asset (#44)

--- packages/snap/src/modules/bitcoin/constants.ts
+++ packages/snap/src/modules/bitcoin/constants.ts
@@ -15,6 +15,6 @@ export enum DataClient {
 }
 
 export enum BtcAsset {
-  Btc = 'bip122:000000000019d6689c085ae165831e93:slip44:0',
-  TBtc = 'bip122:000000000933ea01ad0ee984209779ba:slip44:0',
+  Btc = 'bip122:000000000019d6689c085ae165831e93/slip44:0',
+  TBtc = 'bip122:000000000933ea01ad0ee984209779ba/slip44:0',
 }
