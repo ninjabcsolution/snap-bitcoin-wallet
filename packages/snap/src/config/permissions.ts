c692e8fe1830ea8afcd692a9bcff679746390793
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2024-05-28 11:46:32 +0800

    feat: add while list domain (#71)

--- packages/snap/src/config/permissions.ts
+++ packages/snap/src/config/permissions.ts
@@ -1,69 +1,39 @@
 import { KeyringRpcMethod } from '@metamask/keyring-api';
 
-export const originPermissions = new Map<string, Set<string>>([
-  [
-    'metamask',
-    new Set([
-      // Keyring methods
-      KeyringRpcMethod.ListAccounts,
-      KeyringRpcMethod.GetAccount,
-      KeyringRpcMethod.FilterAccountChains,
-      KeyringRpcMethod.DeleteAccount,
-      KeyringRpcMethod.ListRequests,
-      KeyringRpcMethod.GetRequest,
-      KeyringRpcMethod.SubmitRequest,
-      KeyringRpcMethod.RejectRequest,
-      KeyringRpcMethod.SubmitRequest,
-      // Chain API methods
-      'chain_getBalances',
-      'chain_broadcastTransaction',
-      'chain_getDataForTransaction',
-      'chain_estimateFees',
-    ]),
-  ],
-  [
-    'http://localhost:8000',
-    new Set([
-      // Keyring methods
-      KeyringRpcMethod.ListAccounts,
-      KeyringRpcMethod.GetAccount,
-      KeyringRpcMethod.CreateAccount,
-      KeyringRpcMethod.FilterAccountChains,
-      KeyringRpcMethod.UpdateAccount,
-      KeyringRpcMethod.DeleteAccount,
-      KeyringRpcMethod.ListRequests,
-      KeyringRpcMethod.GetRequest,
-      KeyringRpcMethod.ApproveRequest,
-      KeyringRpcMethod.RejectRequest,
-      KeyringRpcMethod.SubmitRequest,
-      // Chain API methods
-      'chain_getBalances',
-      'chain_createAccount',
-      'chain_broadcastTransaction',
-      'chain_getDataForTransaction',
-      'chain_estimateFees',
-    ]),
-  ],
-  [
-    'https://metamask.github.io',
-    new Set([
-      // Keyring methods
-      KeyringRpcMethod.ListAccounts,
-      KeyringRpcMethod.GetAccount,
-      KeyringRpcMethod.CreateAccount,
-      KeyringRpcMethod.FilterAccountChains,
-      KeyringRpcMethod.UpdateAccount,
-      KeyringRpcMethod.DeleteAccount,
-      KeyringRpcMethod.ListRequests,
-      KeyringRpcMethod.GetRequest,
-      KeyringRpcMethod.ApproveRequest,
-      KeyringRpcMethod.RejectRequest,
-      KeyringRpcMethod.SubmitRequest,
-      // Chain API methods
-      'chain_getBalances',
-      'chain_broadcastTransaction',
-      'chain_getDataForTransaction',
-      'chain_estimateFees',
-    ]),
-  ],
+const allowSet = new Set([
+  // Keyring methods
+  KeyringRpcMethod.ListAccounts,
+  KeyringRpcMethod.GetAccount,
+  KeyringRpcMethod.CreateAccount,
+  KeyringRpcMethod.FilterAccountChains,
+  KeyringRpcMethod.UpdateAccount,
+  KeyringRpcMethod.DeleteAccount,
+  KeyringRpcMethod.ListRequests,
+  KeyringRpcMethod.GetRequest,
+  KeyringRpcMethod.ApproveRequest,
+  KeyringRpcMethod.RejectRequest,
+  KeyringRpcMethod.SubmitRequest,
+  // Chain API methods
+  'chain_getBalances',
+  'chain_broadcastTransaction',
+  'chain_getDataForTransaction',
+  'chain_estimateFees',
 ]);
+
+const allowedOrigins = [
+  'https://metamask.github.io',
+  'https://portfolio.metamask.io',
+  'https://portfolio-builds.metafi-dev.codefi.network',
+  'https://dev.portfolio.metamask.io',
+];
+
+const local = 'http://localhost:8000';
+const metamask = 'metamask';
+
+export const originPermissions = new Map<string, Set<string>>([]);
+
+for (const origin of allowedOrigins) {
+  originPermissions.set(origin, allowSet);
+}
+originPermissions.set(metamask, allowSet);
+originPermissions.set(local, new Set([...allowSet, 'chain_createAccount']));
